348de97f0c92f16830284f17a83ad71ff1b0da51
Alexey Leshchenko <leshchenko@gmail.com>
2022-05-08 13:18:22 +0300

    Increase test coverage

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -1,34 +1,40 @@
 from pathlib import Path
 
+import pytest
+
 from perscache import Cache
 from perscache.storage import LocalFileStorage
 
 
-def test_max_size(tmp_path):
-    MAX_SIZE = 10_000
-
+@pytest.mark.parametrize("max_size", [0, 1000, 10_000])
+def test_max_size(tmp_path, max_size):
     path: Path = tmp_path
 
-    # path should be empty
-    # assert path.stat().st_size == 0
+    cache = Cache(storage=LocalFileStorage(path, max_size=max_size))
 
-
-
-    cache = Cache(storage=LocalFileStorage(path, max_size=MAX_SIZE))
-
+    initial_size = path.stat().st_size
     counter = 0
 
     @cache.cache()
     def get_data(key):
         nonlocal counter
         counter += 1
-        return [counter] * 5000
+        return [counter] * min((max_size // 2), 100)
 
     get_data(1)
     get_data(2)
     get_data(3)
     get_data(4)
 
-    assert path.stat().st_size <= MAX_SIZE
+    assert path.stat().st_size <= max(max_size, initial_size)
 
     assert counter == 4
+
+def test_no_path(tmp_path):
+    path: Path = tmp_path / "new_path"
+    assert not path.exists()
+
+    cache = Cache(storage=LocalFileStorage(path))
+    cache.set("a", 1, cache.serializer, cache.storage)
+
+    assert path.exists()
