80159bc98a3784d2ebde6c40867aef5dbdf6af91
Alexey Leshchenko <leshchenko@gmail.com>
2022-05-18 16:02:48 +0300

    Read env vars from `.env.yaml`

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,16 @@
+import os
+import pathlib
+
 import pytest
+import yaml
 from perscache import Cache
 from perscache.storage import LocalFileStorage
+
+env = pathlib.Path(".env.yaml")
+if env.exists():
+    env_dict = yaml.safe_load(env.read_text())
+    for key, value in env_dict.items():
+        os.environ[key] = value
 
 
 @pytest.fixture
